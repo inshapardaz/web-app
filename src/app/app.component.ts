031b1bbd51b18c9351f6c4ad6ba1c72243ba1e48
Muhammad Farooq <muhammad.farooq@just-eat.com>
2017-11-23 21:55:45 +0000

    Fixed authentication flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,46 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { RouterModule , Router, NavigationStart } from '@angular/router';
-import { TranslateService, LangChangeEvent} from '@ngx-translate/core';
-
+import { RouterModule, Router, NavigationStart } from '@angular/router';
+import { TranslateService, LangChangeEvent } from '@ngx-translate/core';
+import { AuthenticationService } from './authentication.service';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  currentRoute: string = '';
-  isRtl: Boolean = false;
+    currentRoute: String = '';
+    isRtl: Boolean = false;
 
-  constructor (private router: Router, private translate: TranslateService, private titleService: Title) {
-    this.setLanguages();
-    this.router.events
-    .subscribe(event => {
-        if (event instanceof NavigationStart) {
-             this.currentRoute = event.url;
-        }
-    });
-    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
-        this.isRtl = event.lang === 'ur';
-      });
-  }
+    constructor(private router: Router,
+                private translate: TranslateService,
+                private authService: AuthenticationService,
+                private titleService: Title) {
+        this.authService.configureOAuth();
+        this.setLanguages();
+        this.router.events
+            .subscribe(event => {
+                if (event instanceof NavigationStart) {
+                    this.currentRoute = event.url;
+                }
+            });
+        this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+            this.isRtl = event.lang === 'ur';
+        });
+    }
 
-  private setLanguages() {
-    this.translate.addLangs(['en', 'ur']);
-    this.translate.setDefaultLang('en');
+    private setLanguages() {
+        this.translate.addLangs(['en', 'ur']);
+        this.translate.setDefaultLang('en');
 
-    this.translate.get('APP.TITLE').subscribe((res: string) => {
-        this.titleService.setTitle(res);
-    });
+        this.translate.get('APP.TITLE').subscribe((res: string) => {
+            this.titleService.setTitle(res);
+        });
 
-    const browserLang: string = this.translate.getBrowserLang();
-    const selectedLang = localStorage.getItem('ui-lang');
+        const browserLang: string = this.translate.getBrowserLang();
+        const selectedLang = localStorage.getItem('ui-lang');
 
-    this.translate.use(selectedLang ? selectedLang : (browserLang.match(/en|ur/) ? browserLang : 'en'));
-    this.isRtl = selectedLang === 'ur';
-}
-
+        this.translate.use(selectedLang ? selectedLang : (browserLang.match(/en|ur/) ? browserLang : 'en'));
+        this.isRtl = selectedLang === 'ur';
+    }
 }
