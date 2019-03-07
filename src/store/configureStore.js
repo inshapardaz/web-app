c99d67e84510b4853c5c5e29f5a6f4e54b8facca
Muhammad Farooq <muhammad.farooq@just-eat.com>
2019-03-07 13:14:52 +0000

    Added compose

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,5 @@
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history'
 import { routerMiddleware } from 'connected-react-router'
